ff4464d14d348d2481ada1a19267206846399759
Samuca <samuelmadeiradev@gmail.com>
2024-01-24 19:42:45 -0300

    Adicionado opção de category para products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,7 +45,9 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :price)
+    params.require(:product).permit(:name, :description, :price, :category)
   end
+
+  
 end
 
--- db/migrate/20240124215658_add_category_to_produtos.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToProdutos < ActiveRecord::Migration[7.1]
-  def change
-    add_column :produtos, :category, :string
-  end
-end
