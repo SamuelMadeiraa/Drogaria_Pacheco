6eb4c8317716a13e0d22eeb8c990d3e1ab7c3d3e
Samuca <samuelmadeiradev@gmail.com>
2024-01-27 19:45:14 -0300

    Adicionado navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,12 +5,15 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        minhaCor: 'rgb(255, 0, 40)', // Substitua '#ff00ff' pela cor desejada
       },
     },
   },
