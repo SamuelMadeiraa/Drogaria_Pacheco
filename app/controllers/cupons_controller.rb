01365cefefecd85d1b6fe1cb2698fe58b003bbbd
Samuca <samuelmadeiradev@gmail.com>
2024-02-27 13:28:44 -0300

    Adicionado crud de cupom

---
+++ app/controllers/cupons_controller.rb
@@ -0,0 +1,50 @@
+class CuponsController < ApplicationController
+    before_action :set_cupom, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @cupons = Cupom.all
+    end
+  
+    def show
+    end
+  
+    def new
+      @cupom = Cupom.new
+    end
+  
+    def create
+      @cupom = Cupom.new(cupom_params)
+      if @cupom.save
+        redirect_to @cupom, notice: 'Cupom criado com sucesso.'
+      else
+        render :new
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @cupom.update(cupom_params)
+        redirect_to @cupom, notice: 'Cupom atualizado com sucesso.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @cupom.destroy
+      redirect_to cupons_url, notice: 'Cupom excluído com sucesso.'
+    end
+  
+    private
+  
+    def set_cupom
+      @cupom = Cupom.find(params[:id])
+    end
+  
+    def cupom_params
+      params.require(:cupom).permit(:nome, :validade, :percentual_desconto)
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   get '/inicio', to: 'home#index'
   resources :products
   resources :categories
-  resources :copoms
-       
+  resources :cupons        
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
